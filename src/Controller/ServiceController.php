21cfb9e96ca22f47c6ed5be514675b6e71facff3
Yamingue Kingatal Franck <yamking01@gmail.com>
2021-03-20 16:56:08 +0100

    ajout de Anime on scroll

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -6,15 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class ServiceController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/service", name="service")
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('service/index.html.twig', [
+            'controller_name' => 'ServiceController',
         ]);
     }
 }
