2b2aec4078324d0a0d26429467e4c06ab7aec19d
Yamingue Kingatal Franck <yamking01@gmail.com>
2021-03-22 14:58:39 +0100

    listing menbre

--- src/Controller/MenbreController.php
+++ src/Controller/MenbreController.php
@@ -2,19 +2,27 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\MenbreRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MenbreController extends AbstractController
 {
     /**
      * @Route("/menbre", name="menbre")
      */
-    public function index(): Response
+    public function index(Request $req,PaginatorInterface $paginator,MenbreRepository $mr): Response
     {
+        $menbres = $paginator->paginate(
+            $mr->FindAllPaginate(), /* query NOT result */
+            $req->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
         return $this->render('menbre/index.html.twig', [
-            'controller_name' => 'MenbreController',
+            'menbres' => $menbres,
         ]);
     }
 }
